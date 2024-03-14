673f26dd230d6f650148e13ea2e4c147c5fdb825
manuellaN <manuella.nogueira02@gmail.com>
2024-03-14 09:49:18 -0300

    mensagem contém nome

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
+var nome = document.getElementById("nome");
+
 
 function alertar(event) {
-    alert("Você clicou no botão!" + event.value);
+    alert("Você clicou no botão! " + nome.value);
 }
